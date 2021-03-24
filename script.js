d5787b18d69601f9ce41620413a1aba0cd853b0e
wendrias <39842984+wendrias@users.noreply.github.com>
2021-03-24 10:47:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function start() {
     xhttp3.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("p").innerHTML = this.responseText;
-       }
+       } else { document.getElementById("p").innerHTML = "it's Broken :(";}
     };
-    xhttp3.open("GET", "https://random-data-api.com/api/color/random_color", true);
+    xhttp3.open("GET", "https://api.musixmatch.com/ws/1.1/track.search?q_track=my&q_artist=justin&apikey=d4c32e56bd2059bf8093e93376444722", true);
     xhttp3.send();
 }
